325bd55130dfa65c43301c16268043be102ef82c
En-2863 <ewea20221310@gmail.com>
2023-12-16 19:26:46 +0800

    modify gitignore

--- judge/run.sh
+++ judge/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+BASE_DIR=$(dirname $(realpath $0))
+
+# For my local windows machine
+# docker run --rm -it \
+#    -v C:\\Users\\En.Avenue\\Desktop\\lab2-handout\\judge\\global:/mnt/global_space \
+#    -v C:\\Users\\En.Avenue\\Desktop\\lab2-handout\\judge\\user:/mnt/user_space \
+#    --cpus 4 --pids-limit 512 --memory 8g --network none --memory-swap -1 \
+#    --hostname container --add-host="container:127.0.0.1" \
+#    -e TOKEN=1:submission \
+#    lab2_submitter_local
+
+# For Linux
 docker run --rm -it \
-    -v C:\\Users\\En.Avenue\\Desktop\\lab2-handout\\judge\\global:/mnt/global_space \
-    -v C:\\Users\\En.Avenue\\Desktop\\lab2-handout\\judge\\user:/mnt/user_space \
+    -v $BASE_DIR/global:/mnt/global_space \
+    -v $BASE_DIR/user:/mnt/user_space \
     --cpus 4 --pids-limit 512 --memory 8g --network none --memory-swap -1 \
     --hostname container --add-host="container:127.0.0.1" \
     -e TOKEN=1:submission \
