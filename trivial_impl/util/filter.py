9aa78de8a3c43a3a0f17cb0ee67c4e642774ecbe
witherstrike <yinqiuheng@yeah.net>
2023-12-21 22:29:28 +0800

    update filter, add ternary check

--- trivial_impl/util/filter.py
+++ trivial_impl/util/filter.py
@@ -1,9 +1,17 @@
-import logging
+def commutative_and_ternary(expression) -> bool:
+    """
+    Given a statement check if it satisfies:
+    1. If the operator is commutative, size of left operand <= right operand
+    2. If the operator is ternary, expression in true condition doesn't equal to expression in false condition
 
+    Args:
+        Stmts (List(Symbol)):
+            A list representing a statement, e.g. [[* Start Start]]
 
-# logging.basicConfig(filename="filter.log", level='INFO')
+    Returns:
+        True if the expression and its sub-expression satisfie all the rules above, else otherwise
+    """
 
-def commutative(expression) -> bool:
     def commutative_dfs(now_expr) -> (bool, int):
         results = []
         sizes = []
@@ -17,9 +25,13 @@ def commutative(expression) -> bool:
             results.append(res)
             sizes.append(size)
 
-        if now_expr[0] in ['+', '*']:
+        if now_expr[0] in ['+', '*', 'bvand', 'bvor', 'bvxor', 'bvadd', 'and', 'or']:
             assert len(now_expr) >= 3
-            return results[1] and results[2] and sizes[1] <= sizes[2], sum(sizes)
+            return all(results) and sizes[1] <= sizes[2], sum(sizes)
+
+        if expression[0] in ['if0', 'ite']:
+            if type(expression[2]) is tuple and type(expression[3]) is tuple:
+                return all(results) and expression[2] != expression[3], sum(sizes)
 
         return all(results), sum(sizes)
 
@@ -27,7 +39,10 @@ def commutative(expression) -> bool:
 
 
 class ExprFilter:
-
+    """
+        The ExprFilter class, a function class that can be called on an expression and return True if
+        it satisfies all the rules added to it
+    """
     def add_rule(self, rule):
         self.rules.append(rule)
 
@@ -37,7 +52,7 @@ class ExprFilter:
 
     def __init__(self):
         self.rules = []
-        self.rules.append(commutative)
+        self.rules.append(commutative_and_ternary)
         pass
 
 
